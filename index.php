ce552bb55555a01c0381a9165eb41f6b2ee61e77
Nathan Hulsey <Nathan@Nathans-MacBook-Pro.local>
2017-03-04 06:02:23 -0500

    Had to rename the project taxonomy. Trying to get the project archive to work. Don't know if this was actually correct because I still can't get the projects to populate on the projects-archive page...

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
  */
 
 $context = Timber::get_context();
-$context['posts'] = Timber::get_posts();
-$context['foo'] = 'bar';
+$context['projects'] = Timber::get_posts('projects');
+$context['project_types'] = Timber::get_terms('project-type');
 $templates = array( 'index.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'home.twig' );
